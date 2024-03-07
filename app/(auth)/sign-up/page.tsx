20417e5d647e64f5a47e0158840854edf0a3b981
João Gonçalves <joao_i0807@outlook.com>
2024-03-07 16:14:36 -0300

    feat: atualizando codigo para a development

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,19 +1,51 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from '@/app/images/logo.jpg'
+'use client'
+import React, { useState, FormEvent } from 'react';
 
-const page = () => {
+export default function Page() {
+  const [apiResponse, setApiResponse] = useState<string>('');
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    try {
+      const formData = new FormData(event.currentTarget);
+      const response = await fetch('http://localhost:3001/users/createUser', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to submit the data. Please try again.');
+      }
+
+      // Agora a resposta do servidor será exibida corretamente
+      const data = await response.json();
+      setApiResponse(`Usuário cadastrado com sucesso. ID: ${data.id}`);
+    } catch (error) {
+      console.error('Error during form submission:', error);
+      setApiResponse('Erro ao cadastrar usuário. Por favor, tente novamente.');
+    }
+  }
+
   return (
-    <div>
-        <Image src={logo} alt='logo' width={400} className=' bg-black'  />
-        <div id="g_id_onload"
-            data-client_id="YOUR_GOOGLE_CLIENT_ID"
-            data-login_uri="https://your.domain/your_login_endpoint"
-            data-your_own_param_1_to_login="any_value"
-            data-your_own_param_2_to_login="any_value">
-        </div>
-    </div>
-  )
-}
+    <div className='flex justify-around items-center'>
+      <div>
+        <h1>IMAGEM - LOGO</h1>
+      </div>
 
-export default page
+      <div className=' flex flex-col justify-between'>
+        <h1 className=' text-7xl font-bold' >Acontecendo agora</h1>
+        <h2 className='text-4xl font-bold'>Inscreva-se hoje</h2>
+        <form onSubmit={onSubmit} className='flex flex-col gap-3 text-black'>
+          <input type="text" name="email" />
+          <input type="text" name="username" />
+          <input type="text" name="password" />
+          <input type="text" name="imgUrl" />
+          <button className='bg-white' type="submit">Submit</button>
+        </form>
+      </div>
+      {/* Exiba a resposta da API ou mensagens de erro/sucesso */}
+      {apiResponse && <p>{apiResponse}</p>}
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    },
+    screens: {
+      xs: "375px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
